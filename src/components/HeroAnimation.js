c144428780c91cf477d43b3d49da1096c2564289
Mohammad-Rah1m <rahimvhangezi5@gmail.com>
2024-12-06 01:58:14 +0500

    lottie animation added back

--- src/components/HeroAnimation.js
+++ src/components/HeroAnimation.js
@@ -1,18 +1,20 @@
-"use client"
-import React from 'react'
-import Lottie from 'lottie-react';
-import HeroAnimationData from "../../public/lottie/hero-animation.json"
+"use client";
+import React from "react";
+import Lottie from "lottie-react";
+import HeroAnimationData from "../../public/lottie/hero-animation.json";
 
 const HeroAnimation = () => {
   return (
+    <div>
       <Lottie
         animationData={HeroAnimationData}
         loop={true}
         autoplay={true}
         // style={{ width: '300px', height: '200px' }}
         className="w-[80%] h-auto z-10"
-        />
-  )
-}
+      />
+    </div>
+  );
+};
 
-export default HeroAnimation
+export default HeroAnimation;
